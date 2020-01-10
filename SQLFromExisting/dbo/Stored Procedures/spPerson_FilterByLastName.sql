81867e74bae693bbdc3cc92984b17be749e61dd0
david colvin <davidmcolvin@gmail.com>
2020-01-09 21:13:09 -0500

    Initial commit of demo showing sql server database project

---
+++ SQLFromExisting/dbo/Stored Procedures/spPerson_FilterByLastName.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spPerson_FilterByLastName]
+  @LastName nvarchar(50)
+AS
+begin
+  SELECT p.*
+  from dbo.Person p
+  where p.LastName = @LastName;
+end
---
+++ SQLProject/dbo/Views/FullPerson.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[FullPerson]
+  AS 
+  select 
+  [p].[FirstName], 
+  [p].[LastName] , 
+  [a].[Id] AddressId, 
+  [a].[StreetAddress], 
+  [a].[City], 
+  [a].[State], 
+  [a].[ZipCode]
+  from dbo.Person p
+  left join dbo.Address a
+  on a.PersonId = p.[PersonId]
+
